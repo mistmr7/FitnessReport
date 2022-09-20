3c954e72b33da8015b83f0a1126bbeacffe9230b
Michael Mistarz <michael.mistarz@gmail.com>
2022-09-20 06:31:04 -0600

    updated secret key

--- FitnessReport/settings.py
+++ FitnessReport/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^^ln@(u%$bt24=xt$h5$ajdg@$h^9n9y*lr28v2xinmkc-w7#)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
